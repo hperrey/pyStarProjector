a8ea1506645cf0d35199254faa0334bb232b8ed9
Hanno Perrey <hanno.perrey@desy.de>
2016-11-30 17:38:51 +0100

    added star object for handling star info

--- pyStarProject.py
+++ pyStarProject.py
@@ -3,6 +3,21 @@ import numpy
 import dodecahedron
 import csv
 
+class Star:
+    """ class to store a star's data from the catalogue database """
+    id = -1 # The database primary key.
+    ra = 0 # The star's right ascension, for epoch and equinox 2000.0.
+    dec = 0 # The star's declination, for epoch and equinox 2000.0.
+    name = "" # A common name for the star,
+    mag = 0 # The star's apparent visual magnitude.
+    def __init__(self, db):
+        self.id = db['id']
+        self.ra = db['ra']
+        self.dec = db['dec']
+        self.name = db['proper']
+        self.mag = db['mag']
+        
+        
 if __name__ == "__main__":
     print("Dodecahedron with vertices on poles")
     vertexDodec = dodecahedron.Dodecahedron(withFacesOnPoles=False)
@@ -10,9 +25,13 @@ if __name__ == "__main__":
         faceVec = vertexDodec.getFaceCtr(i)
         print("Face %2d: %s" % (i, faceVec))
 
+    starlist = []
     with open('HYG-Database/hygdata_v3.csv', 'r') as csvfile:
         starreader = csv.DictReader(csvfile, delimiter=',')
         for row in starreader:
-            print (row)
-            if starreader.line_num > 20:
+            starlist.append(Star(row))
+            if starreader.line_num > 200:
                 break
+    
+
+            
