a9de854df5d41fbaa6fcab698e786bd10b9f4045
Hanno Perrey <hanno.perrey@desy.de>
2016-11-30 17:18:33 +0100

    simple dump of csv star file

--- pyStarProject.py
+++ pyStarProject.py
@@ -9,3 +9,10 @@ if __name__ == "__main__":
     for i in range(12):
         faceVec = vertexDodec.getFaceCtr(i)
         print("Face %2d: %s" % (i, faceVec))
+
+    with open('HYG-Database/hygdata_v3.csv', 'r') as csvfile:
+        starreader = csv.DictReader(csvfile, delimiter=',')
+        for row in starreader:
+            print (row)
+            if starreader.line_num > 20:
+                break
