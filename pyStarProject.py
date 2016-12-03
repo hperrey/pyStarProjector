60853eac3a48fedb2f39aff89c6fff61b1de8afc
Hanno Perrey <hanno.perrey@desy.de>
2016-12-03 15:51:57 +0100

    added 3D coordinates of star on sphere and 3D plot, aded arguments for filtering, added plots for selected stars stats

--- pyStarProject.py
+++ pyStarProject.py
@@ -2,8 +2,11 @@
 import numpy
 import dodecahedron
 import csv
+import argparse
 
+import math
 import numpy as np
+
 import matplotlib.pyplot as plt
 
 class Star:
@@ -22,20 +25,59 @@ class Star:
         
         
 if __name__ == "__main__":
+    # argument parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--nentries", help="max number of entries to read from the catalogue",
+                        type=int, default=-1, action="store")
+    parser.add_argument("--mag", help="largest apparent magnitude to still consider for selecting stars from catalogue",
+                        type=int, default=6.5, action="store")
+    args = parser.parse_args()
+
+    # dodecahedron test
     print("Dodecahedron with vertices on poles")
     vertexDodec = dodecahedron.Dodecahedron(withFacesOnPoles=False)
     for i in range(12):
         faceVec = vertexDodec.getFaceCtr(i)
         print("Face %2d: %s" % (i, faceVec))
 
+    # read in star catalogue data
     starlist = []
     with open('HYG-Database/hygdata_v3.csv', 'r') as csvfile:
         starreader = csv.DictReader(csvfile, delimiter=',')
         for row in starreader:
-            starlist.append(Star(row))
-            if starreader.line_num > 2000:
+            s = Star(row)
+            if s.mag < args.mag:
+                starlist.append(s)
+            if args.nentries > 0 and len(starlist) > args.nentries:
+                print ("Reached requested maximum number of events: {}".format(args.nentries))
                 break
 
-    x = np.array([s.ra for s in starlist])
-    plt.hist(x, 50)
+    print ("Loaded {} stars into memory".format(len(starlist)))
+            
+    # plot some of the relevant star data
+    plt.figure()
+    plt.hist(np.array([s.dec for s in starlist]), 50)
+    plt.xlabel("Dec")
+    plt.figure()
+    plt.hist(np.array([s.ra for s in starlist]), 50)
+    plt.xlabel("RA")
+    plt.figure()
+    plt.hist(np.array([s.mag for s in starlist]), 50)
+    plt.xlabel("mag")
+    
+    locations = []
+    for star in starlist:
+        # convert the ra and dec into angles in rad
+        phi = (star.ra/24) * 2 * math.pi
+        rho = (star.dec/90) * math.pi
+        vec = np.array([math.sin(rho) * math.cos(phi), #x
+                       math.sin(rho) * math.sin(phi), #y
+                       math.cos(rho)] #z
+                       )
+        locations.append(vec)
+
+    from mpl_toolkits.mplot3d import Axes3D
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter([l[0] for l in locations],[l[1] for l in locations], [l[2] for l in locations])
     plt.show()
